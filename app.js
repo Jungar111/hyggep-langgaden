aca50d8f5e5a22bbc0d2adb24a0ea672483511b4
Jungar111 <asger@tang.dk>
2021-10-13 01:06:37 +0200

    comit

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const app = require('express')();
+let http = require('http');
+let fs = require('fs');
 
-const PORT = process.env.PORT || 3000;
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
 
-app.get("", (req, res) =>{
-    res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-    console.log('app up! ${PORT}');
-});
+http.createServer(handleRequest).listen(8000);
